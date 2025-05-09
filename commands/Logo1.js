0b7f1a9545d86eaf86cccaa911104ae384df3001
devpopkid <talkdroveke@gmail.com>
2025-05-09 15:55:01 +0300

    Update Logo1.js

--- commands/Logo1.js
+++ commands/Logo1.js
@@ -65,15 +65,15 @@ keith({ nomCom: "logo", categorie: "Search", reaction: "✋" }, async (dest, zk,
 49⊷ Anonymous hacker (avatar cyan neon)
 50⊷ Birthday cake
 
-> 𝐁𝐄𝐋𝐓𝐀𝐇 𝐌𝐃`;
+> 𝐏𝐎𝐏𝐊𝐈𝐃 𝐌𝐃`;
 
     const contextInfo = {
       mentionedJid: [ms.sender], // Mention the sender
       externalAdReply: {
-        title: "𝐁𝐄𝐋𝐓𝐀𝐇 𝐌𝐃",
+        title: "𝐏𝐎𝐏𝐊𝐈𝐃 𝐌𝐃",
         body: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʙᴇʟᴛᴀʜ ʜᴀᴄᴋɪɴɢ ᴛᴇᴀᴍ",
         thumbnailUrl: "https://telegra.ph/file/dcce2ddee6cc7597c859a.jpg",
-        sourceUrl: "https://whatsapp.com/channel/0029VaRHDBKKmCPKp9B2uH2F",
+        sourceUrl: "https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l",
         mediaType: 1,
         renderLargerThumbnail: true,
       },
